0323f4a35660c0969d34f3b559c17dff139f417a
Christophe Ribeiro <christophe@ribeiro.io>
2024-12-11 15:58:01 +0100

    feat: remove read and write command

--- src/cli.ts
+++ src/cli.ts
@@ -1,36 +1,5 @@
 import { boolean, command, run as runCommand, string } from "@drizzle-team/brocli"
 
-/**
- * Read a *.conf file
- */
-export const read = command({
-  name: "read",
-  help: "Read a *.conf file",
-  options: {
-    input: string().alias("i").required().desc("The *.conf file to read"),
-    print: boolean().alias("p").desc("Print the generated config").default(false),
-  },
-  handler: opts => {
-    console.log(opts)
-  },
-})
-
-/**
- * Write a *.ts from a *.conf
- */
-export const write = command({
-  name: "write",
-  help: "Write a *.ts from a *.conf",
-  options: {
-    input: string().alias("i").required().desc("The *.conf file to read"),
-    output: string().alias("o").required().desc("The *.ts file to write"),
-    print: boolean().alias("p").desc("Print the generated config").default(false),
-  },
-  handler: opts => {
-    console.log(opts)
-  },
-})
-
 export const run = command({
   name: "run",
   help: "Read a *.conf and write a *.ts",
@@ -44,8 +13,8 @@ export const run = command({
   },
 })
 
-runCommand([read, write, run], {
+runCommand([run], {
   name: "betterconf",
-  description: "[*].conf -> [*].ts",
+  description: "From .conf to .ts",
   version: "0.1.0",
 })
